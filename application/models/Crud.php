c4e58b42738daaeb91f99aaaf01b64a475c57a96
neeraj10 <neeraj20061988@gmail.com>
2022-02-17 17:01:17 +0530

    Add files via upload

--- application/models/Crud.php
+++ application/models/Crud.php
@@ -2,18 +2,32 @@
  class Crud extends CI_Model{ 
      
      
- function insert($table,$data) 
-             { 
-                         $result= $this->db->insert($table, $data); 
-                         return $result; 
-             } 
+            function insert($sap_id,$host_name,$mac_address,$loopback) 
+            { 
+               $data = array(
+                  'Sapid'=>$sap_id,
+                  'Hostname'=>$host_name,
+                  'Loopback'=>$loopback,
+                  'mac_address'=>$mac_address,
+                
              
-             
+             );
+             if ($this->db->insert('router_details', $data)) {
+                return 'success';
+             } else {
+                return 'error';
+             }
+            } 
              
              function update($table,$id,$data) 
              { 
-                         $this->db->where('id', $id); 
-                         return $this->db->update($table,$data); 
+               $this->db->where('id', $id); 
+               if ($this->db->update($table,$data)) {
+                  return 'success';
+               } else {
+                  return 'error';
+               }
+               //return $this->db->update($table,$data); 
              } 
    
    
